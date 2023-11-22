5520ba0d6b5b22b8a06aa5184c9ad8a6c6603f4e
Taavi Kalaluka <taavi.k@protonmail.com>
2023-11-22 10:39:19 +0200

    removed hello world route

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
 from app import app
 from app.bot import get_response
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+from flask import request, jsonify
 
 # Route with prompt for the chatbot goes here
+@app.route('/bot', methods=['POST'])
+def bot_response():
+    prompt = request.json['prompt']
+    response = get_response(prompt)
+    return jsonify({'response': response})
