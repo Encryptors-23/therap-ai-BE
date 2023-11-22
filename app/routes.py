a61a61fe92cfbb3cdb59307ac10d5476f4f55ba2
Taavi Kalaluka <taavi.k@protonmail.com>
2023-11-22 11:00:12 +0200

    added hardcoded response for testing purposes

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,16 @@
 from app import app
-from app.bot import get_response
+# test_respose is a hardcoded response for testing purposes
+from app.bot import get_response, test_response
 from flask import request, jsonify
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 # Route with prompt for the chatbot goes here
 @app.route('/bot', methods=['POST'])
 def bot_response():
     prompt = request.json['prompt']
-    response = get_response(prompt)
-    return jsonify({'response': response})
+    response = test_response(prompt)
+    #return jsonify({'response': response})
+    return jsonify(response)
