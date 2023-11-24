dbd69b750db97e4769c5934c7e40979349b201d7
Taavi Kalaluka <taavi.k@protonmail.com>
2023-11-24 20:36:13 +0200

    minor fixes

--- app/bot.py
+++ app/bot.py
@@ -9,7 +9,7 @@ def test_response(prompt):
     "response": "It sounds like you're feeling overwhelmed with the amount of homework you have and the limited time you have to complete it. School-related anxiety is a common issue, and it's important to remember that you're not alone in feeling this way. Here are a few strategies that may help:\n\n1. Prioritize: Break down your tasks into smaller, manageable chunks. Prioritize your assignments based on their due dates and importance.\n\n2. Time management: Create a schedule or use a planner to allocate specific time slots for each task. This can help you stay organized and ensure you're making progress on each assignment.\n\n3. Reach out for support: Don't hesitate to seek help and support from your teachers, classmates, or even a school counselor. They may be able to provide guidance or extra resources to assist you.\n\n4. Take breaks: It's essential to take regular breaks to recharge and avoid burnout. Engaging in activities you enjoy during these breaks can help reduce stress and improve focus when you return to your work.\n\nRemember, it's also important to maintain a healthy work-life balance. Be kind to yourself and practice self-care. You got this!"
     }
     
-    return response
+    return response["response"]
 
 def get_response(prompt):
     response = client.chat.completions.create(
--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,10 @@ from app.bot import get_response
 from flask import request, jsonify
 from app.encryption import encrypt, decrypt 
 
+@app.route('/', methods=['GET'])
+def index():
+    return "This is the backend for the chatbot."
+
 # Route with prompt for the chatbot goes here
 @app.route('/bot', methods=['POST'])
 def bot_response():
@@ -10,8 +14,4 @@ def bot_response():
     decrypted_user_prompt = decrypt(encrypted_user_prompt)
     bot_response = get_response(decrypted_user_prompt)
     encrypted_bot_response = encrypt(bot_response)
-    return jsonify({'encrypted_response': encrypted_bot_response})
-
-
-
-
+    return jsonify({'response': encrypted_bot_response})
