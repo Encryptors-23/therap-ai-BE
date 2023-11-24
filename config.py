7e0ad5db1c9e6534a51e00bd36771ec50ae5dfc6
Taavi Kalaluka <taavi.k@protonmail.com>
2023-11-24 18:58:26 +0200

    removed length check

--- config.py
+++ config.py
@@ -3,16 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv('.env')
 
-# SECRET_KEY = os.environ.get('OPENAI_API_KEY') 
-# if SECRET_KEY is None:
-#     raise ValueError("Error: OPENAI_API_KEY is not set. Please set it in the .env file.")
+SECRET_KEY = os.environ.get('OPENAI_API_KEY') 
+if SECRET_KEY is None:
+    raise ValueError("Error: OPENAI_API_KEY is not set. Please set it in the .env file.")
 
 AES_KEY = os.environ.get("AES_KEY")
 if AES_KEY is None:
     raise ValueError("Error: AES_KEY is not set. Please set it in the .env file.")
-
-if len(AES_KEY) != 32:
-    raise ValueError("Error: AES_KEY should be 32 bytes long.")
 
 IV = os.environ.get("IV")
 if IV is None:
